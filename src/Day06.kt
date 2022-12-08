9fbee3e931c260a6521ed7ef8c0685e148b77145
Andreas Denkmayr <andreas.denkmayr@huspy.io>
2022-12-08 19:00:02 +0100

    Day 6 solution

---
+++ src/Day06.kt
@@ -0,0 +1,40 @@
+fun main() {
+    // test if implementation meets criteria from the description, like:
+    val testInput = readInput("Day06_test")
+    check(part1(testInput[0]) == 7)
+    check(part1(testInput[1]) == 5)
+    check(part1(testInput[2]) == 6)
+    check(part1(testInput[3]) == 10)
+    check(part1(testInput[4]) == 11)
+
+    check(part2(testInput[0]) == 19)
+    check(part2(testInput[1]) == 23)
+    check(part2(testInput[2]) == 23)
+    check(part2(testInput[3]) == 29)
+    check(part2(testInput[4]) == 26)
+
+    val input = readInput("Day06").first()
+    println("Part 1: ${part1(input)}")
+    println("Part 2: ${part2(input)}")
+}
+
+private fun part1(input: String): Int = signalDetected(input, 4)
+
+private fun part2(input: String): Int = signalDetected(input, 14)
+
+private fun signalDetected(input: String, uniqueCharacters: Int): Int {
+    val signals = emptyList<Char>().toMutableList()
+
+    input.forEachIndexed { index, char ->
+        signals.add(char)
+
+        if (index >= uniqueCharacters) {
+            signals.removeAt(0)
+        }
+
+        if (signals.toSet().size == uniqueCharacters) {
+            return index + 1
+        }
+    }
+    return input.length
+}
